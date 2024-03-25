4c0f4fe187cdfb48227d8b38de6cc2b75b2c7ad3
mishok2004 <mishok2004@mail.ru>
2024-03-25 14:26:31 +0300

    add form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,7 @@
 import './App.css'
+import BookForm from './components/BookForm/BookForm'
+import BookList from './components/BookList/BookList'
+import Filter from './components/Filter/Filter'
 
 function App() {
   return (
@@ -6,6 +9,15 @@ function App() {
       <header className="app-header">
         <h1>Liblery app</h1>
       </header>
+      <main className="app-main">
+        <div className="app-left-colump">
+          <BookList />
+        </div>
+        <div className="app-right-colum">
+          <Filter />
+          <BookForm />
+        </div>
+      </main>
     </div>
   )
 }
