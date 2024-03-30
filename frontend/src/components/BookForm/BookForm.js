1073853a97c5b042ef6cba6c0f53583fa8fca699
mishok2004 <mishok2004@mail.ru>
2024-03-30 23:38:34 +0300

    add uuid

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { useDispatch } from 'react-redux'
+import { v4 as uuidv4 } from 'uuid'
 import { addBook } from '../../redux/books/actionCreators'
 import './BookForm.css'
 
@@ -12,6 +13,7 @@ const BookForm = () => {
     e.preventDefault()
     if (title && author) {
       const book = {
+        id: uuidv4(),
         title,
         author,
       }
