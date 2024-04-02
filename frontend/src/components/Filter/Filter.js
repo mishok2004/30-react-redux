b294a3fdbc3c5e283fc6de7ba0f3b88e2551da16
mishok2004 <mishok2004@mail.ru>
2024-04-03 00:01:58 +0300

    add filterSlice

--- frontend/src/components/Filter/Filter.js
+++ frontend/src/components/Filter/Filter.js
@@ -1,10 +1,25 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { selectFilterText } from '../../redux/filters/model/selector/filterSelector'
+import { setTitleFilter } from '../../redux/filters/model/slices/filterSlice'
+
 import './Filter.css'
 
 const Filter = () => {
+  const titleFilter = useSelector(selectFilterText)
+
+  const dispatch = useDispatch()
+  const handleTitleFilter = (e) => {
+    dispatch(setTitleFilter(e))
+  }
   return (
     <div className="app-block filter">
       <div className="filter-grouo">
-        <input type="text" placeholder="Filter by title ..."></input>
+        <input
+          value={titleFilter}
+          onChange={(e) => handleTitleFilter(e.target.value)}
+          type="text"
+          placeholder="Filter by title ..."
+        ></input>
       </div>
     </div>
   )
